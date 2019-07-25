633fab903feffb6e40de7600c8f98c5a860c6a3a
Sagar Gaikwad <sagargaikwad966@gmail.com>
2019-07-25 18:06:35 +0530

    resolve

--- src/main/java/com/ingbank/banking/controller/OtpController.java
+++ src/main/java/com/ingbank/banking/controller/OtpController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,7 @@ import com.ingbank.banking.service.OtpService;
 
 @RestController
 @RequestMapping("/otp")
+@CrossOrigin
 public class OtpController {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
