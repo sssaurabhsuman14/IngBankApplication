1c53dc53ed136fcc7d5af35c2a15038dc19fb0fa
Sagar Gaikwad <sagargaikwad966@gmail.com>
2019-07-25 17:47:57 +0530

    integration 2

---
+++ src/test/java/com/ingbank/banking/controller/TransactionControllerTest.java
@@ -0,0 +1,10 @@
+package com.ingbank.banking.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TransactionControllerTest 
+{
+
+}
